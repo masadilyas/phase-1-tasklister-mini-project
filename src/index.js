c1384b6859b7c3f4a1fa1861e12ac6fca66cca41
Massad Ilyas <+>
2023-03-23 16:50:37 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+function addList(){
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', newList)}
+
+function newList(e){
+  const li = document.createElement('li')
+ 
+const theList = document.getElementById('tasks')
+theList.appendChild(li)
+li.textContent = e.target['new-task-description'].value
+e.preventDefault()
+} 
+addList()
